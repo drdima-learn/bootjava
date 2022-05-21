88740d36c4c5cf30458d53571947987676113559
Dmitry <drdima2@gmail.com>
2022-05-21 15:52:34 +0300

    rest controller

---
+++ src/main/java/ru/javaops/bootjava/util/ValidationUtil.java
@@ -0,0 +1,24 @@
+package ru.javaops.bootjava.util;
+
+import ru.javaops.bootjava.model.BaseEntity;
+
+public class ValidationUtil {
+
+    public static void checkNew(BaseEntity entity) {
+        if (!entity.isNew()) {
+            throw new IllegalArgumentException(entity + "must be new (id=null)");
+        }
+    }
+
+    public static void assureIdConsistent(BaseEntity entity, int id) {
+        if (entity.isNew()) {
+            entity.setId(id);
+        } else {
+            if (entity.id() != id) {
+                throw new IllegalArgumentException(entity + "must has id=" + id);
+            }
+
+        }
+
+    }
+}
